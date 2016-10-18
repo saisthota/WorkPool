4fd6fcb1b33def44007536e1dfaf6d007e8c0feb
saisthota <me@saisthota.com>
2016-10-17 21:50:24 -0700

    Changes

--- index.js
+++ index.js
@@ -9,23 +9,33 @@ server.use(bodyParser.urlencoded({ extended: true }));
 
 /* Set a route for listening request */
 server.get('/estimates', function(req, res, next) {
-    var params = "Start latlng / end latlng";
-    var token = "SERVER-TOKEN-HERE"
+    /*var params = "Start latlng / end latlng";
+    var token = "SERVER-TOKEN-HERE"*/
 
     var params = "start_latitude=37.351374&start_longitude=-121.992940&end_latitude=37.434650&end_longitude=-121.920546";
     var token = "JaFBwmbHYOT2k0-bU4HNsFfBcEN00cfZhIevShBq"
+
+    //https://api.uber.com/v1/estimates/time
+
     http({
         url: "https://api.uber.com/v1/estimates/price?"+params,
         headers: {"Authorization": "Token "+token}
     },
+
         function (error, response, body) {
             if(!error && response.statusCode == 200) {
                 var estimates = JSON.parse(body);
-                var pool = {
-                    "estimate" : estimates.prices[0].estimate,
-                    "high" : estimates.prices[0].high_estimate
-                };
-                res.send(200, pool);
+                if(estimates.prices[0].display_name=="POOL") {
+                    var pool = {
+                        "estimate": estimates.prices[0].estimate,
+                        "eta": "estimated time",
+                        "high": estimates.prices[0].high_estimate,
+                    };
+                    res.send(200, pool);
+                } else {
+                    res.send(404, "No uberPOOL found nearby!");
+                }
+
             }
         })
 });
