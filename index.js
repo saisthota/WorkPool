545d2bdd02f37f3fa9c0d23b1ae65f3b2ba24afc
saisthota <me@saisthota.com>
2016-10-19 21:47:04 -0700

    Refactoring

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ server.use(bodyParser.urlencoded({ extended: true }));
 
 /* Set a route for listening request */
 server.get('/estimates', function(req, res, next) {
-    /*var params = "Start latlng / end latlng";
-    var token = "SERVER-TOKEN-HERE"*/
-
-
-
-    //https://api.uber.com/v1/estimates/time
+    var params = "Start latlng / end latlng";
+    var token = "SERVER-TOKEN-HERE"
 
     var estimates = {
         uri : "https://api.uber.com/v1/estimates/price?"+estimateParams,
@@ -40,37 +36,6 @@ server.get('/estimates', function(req, res, next) {
 
                 })
         })
-
-
-
-
-
-
-
-
-
-/*
-    http({
-        url: "https://api.uber.com/v1/estimates/price?"+params,
-        headers:
-    },
-
-        function (error, response, body) {
-            if(!error && response.statusCode == 200) {
-                var estimates = JSON.parse(body);
-                if(estimates.prices[0].display_name=="POOL") {
-                    var pool = {
-                        "estimate": estimates.prices[0].estimate,
-                        "eta": "estimated time",
-                        "high": estimates.prices[0].high_estimate,
-                    };
-                    res.send(200, pool);
-                } else {
-                    res.send(404, "No uberPOOL found nearby!");
-                }
-
-            }
-        })*/
 });
 
 server.listen(3000, function() {
